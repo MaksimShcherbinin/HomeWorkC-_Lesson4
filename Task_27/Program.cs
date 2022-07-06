fc7e9a056fcbe40f6eb899ed381ca00b56997de5
Maksim <maksim6erbinin@mail.ru>
2022-07-07 00:17:59 +0300

    Added task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int answer = 0;
+
+for(int i = userNumber; i > 0; i = i / 10) answer = answer + i % 10;
+
+Console.WriteLine($"Сумма цифр числа {userNumber} = {answer}");
