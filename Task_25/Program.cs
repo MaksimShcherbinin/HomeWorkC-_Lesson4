bddbc8e0e1e3a1766dc69939fd2fddc4acebc033
Maksim <maksim6erbinin@mail.ru>
2022-07-06 23:59:57 +0300

    Added task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int answer = 1;
+for(int i = 1; i <= numberB; i++) answer = answer*numberA;
+Console.WriteLine($"{numberA}^{numberB} = {answer}");
