2681eba0c622a3329b17a73f37de80dc040ba333
Maksim <maksim6erbinin@mail.ru>
2022-07-07 00:45:26 +0300

    Added task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число массива: ");
+        collection[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++) Console.Write(array[j]);
+}
+FillArray(array);
+PrintArray(array);
