74ba5e3f980b963334339628f090a22edff963d3
Maksim <maksim6erbinin@mail.ru>
2022-07-07 01:38:47 +0300

    added new string binary serch

--- Binary_Search/Program.cs
+++ Binary_Search/Program.cs
@@ -1,11 +1,17 @@
 ﻿Console.WriteLine("Загадай число от 1 до 100: ");
 int n_first = 0, n_last = 100, mid = 50;
-while (n_first <= n_last)
+Console.WriteLine("Число {0}?", mid);
+string? ans = Console.ReadLine();
+if (ans != "yes")
 {
-    mid = (n_first + n_last) / 2;
-    Console.WriteLine("Число больше {0}?", mid);
-    string? ans = Console.ReadLine();
-    if (ans == "yes") n_first = mid + 1;
-    else n_last = mid - 1;
+    while (n_first <= n_last)
+    {
+        mid = (n_first + n_last) / 2;
+        Console.WriteLine("Число больше {0}?", mid);
+        ans = Console.ReadLine();
+        if (ans == "yes") n_first = mid + 1;
+        else n_last = mid - 1;
+    }
 }
+
 Console.WriteLine(mid);
